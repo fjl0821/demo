7cf0f34b5b1bd6a84c576a1c94a543dfee827931
fjl0821 <1154956359@qq.com>
2021-04-15 17:21:10 +0800

    【新增】基础服务层

--- demo/src/main/java/com/van/demo/service/impl/BaseServiceImpl.java
+++ demo/src/main/java/com/van/demo/service/impl/BaseServiceImpl.java
@@ -2,11 +2,13 @@ package com.van.demo.service.impl;
 
 import com.van.demo.service.BaseService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * @author: VaN
  * @since: 2021/3/5 10:32
  * @remark:
  */
+@Service
 public interface BaseServiceImpl <T, ID, R extends JpaRepository<T, ID>> extends BaseService<T, ID> {
 }
